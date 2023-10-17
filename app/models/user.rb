4f280ea8ca23b8ddeb8d550c333681fc99c0e3b3
katfogy <katfogy@gmail.com>
2023-10-17 10:41:22 +0100

    Linters Error fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    has_many :foods
-    has_many :recipes
+  has_many :foods
+  has_many :recipes
 
-    validates :name, preesence: true
+  validates :name, preesence: true
 end
