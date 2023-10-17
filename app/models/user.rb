100ea2952764d288efcae0476874ecf7fea1bd35
afreen shaik <affushaik9493@gmail.com>
2023-10-18 00:07:48 +0530

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :foods, dependent: :destroy
   has_many :recipes, dependent: :destroy
-
+  has_many :recipe_foods, through: :recipes
+  
   validates :name, :email, presence: true
 end
