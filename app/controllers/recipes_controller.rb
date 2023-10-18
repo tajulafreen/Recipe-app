dda6c744879fc948e8b96b13125d1ad61e688c22
afreen shaik <affushaik9493@gmail.com>
2023-10-19 00:35:08 +0530

    add authentication

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,9 @@
 class RecipesController < ApplicationController
+  before_action :authenticate_user!, except: [:is_public_recipes]
+  def public_recipes
+    @public_recipes = Recipe.where(is_public: true).includes(recipe_foods: :food)
+  end
+
   def index
     @user = current_user
     @recipes = @user.recipes
@@ -43,5 +48,15 @@ class RecipesController < ApplicationController
 
   def recipe_params
     params.require(:recipe).permit(:name, :preparation_time_minutes, :cooking_time_minutes, :description, :is_public)
+  end
+
+  def calculate_total_price(recipe)
+    total_price = 0.0
+
+    recipe.recipe_foods.each do |recipe_food|
+      total_price += recipe_food.food.price * recipe_food.quantity
+    end
+
+    total_price
   end
 end
