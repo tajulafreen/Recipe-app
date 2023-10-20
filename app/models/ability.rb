3fbeb599aa994543f292e7b7e84988f2c70cd370
afreen shaik <affushaik9493@gmail.com>
2023-10-20 23:04:42 +0530

    update ability file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,9 +2,10 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new # Guest user
     can :read, :all
     can :manage, Food, user_id: user.id
     can :manage, Recipe, user_id: user.id
-    can :manage, RecipeFood, user_id: user.id
+    can :manage, RecipeFood, recipe: { user_id: user.id }
   end
 end
