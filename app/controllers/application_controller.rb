2934eac430629b5c8d400f0e23d604eab0f1cf2a
afreen shaik <affushaik9493@gmail.com>
2023-10-20 23:04:00 +0530

    add rescue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # Customize redirect hooks
+  rescue_from CanCan::AccessDenied do |exception|
+    
+  redirect_to root_path, alert: "Access Denied: #{exception.message}"
+  end
+
   def after_sign_in_path_for(_resource)
     users_url
   end
